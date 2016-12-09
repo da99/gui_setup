47807231992d5c990d29e03ff1c74ef8c9b3e77b
da99 <i-hate-spam-1234567@mailinator.com>
2016-12-08 18:26:49 -0600

    Development checkpoint.

--- bin/public/reload-browser/_.sh
+++ bin/public/reload-browser/_.sh
@@ -21,7 +21,7 @@ reload-browser () {
 
     for WIN_ID in $win_id ; do
       local +x RELOAD="xdotool windowfocus --sync $WIN_ID  key --clearmodifiers F5"
-      mksh_setup BOLD "=== Reloaded: $WIN_ID -> {{$(echo "$WINDOWS" | grep $(printf 0x0%x $WIN_ID) || echo "[unknown window]")}}"
+      sh_color BOLD "=== Reloaded: $WIN_ID -> {{$(echo "$WINDOWS" | grep $(printf 0x0%x $WIN_ID) || echo "[unknown window]")}}"
       $RELOAD
       sleep 0.2 # If there is delay, the window will lose focus and the 'key press' (F5) may not be
                 # passed on the window (ie racing issue between focusing and key press.)
